b83459868dc54e0ed01e956a585c90b627034438
AJlaxBTpycax <trickysenya@yandex.ru>
2025-05-28 15:32:22 +0300

    Novel.cs

--- Новелла/Novel.cs
+++ Новелла/Novel.cs
@@ -1,300 +1,207 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
-namespace Task1
+class Program
 {
-    class Program
+    static string playerName = "Детектив";
+    static int creds = 50;
+    static int reputation = 0;
+    static bool hasWeapon = false;
+    static bool knowsTruth = false;
+    static List<string> clues = new List<string>();
+    static Random rand = new Random();
+
+    static void Main()
     {
-        static bool hasKey = false;
-        static bool knowsSecret = false;
-        static bool hasMap = false;
-        static bool metAlly = false;
-        static int trustPoints = 0;
-        static string playerName = "";
+        Console.Title = "NEURO CITY: Бесконечный цикл";
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("╔══════════════════════════════╗");
+        Console.WriteLine("║   NEURO CITY 2084            ║");
+        Console.WriteLine("║   Бесконечный цикл          ║");
+        Console.WriteLine("╚══════════════════════════════╝\n");
+        Console.ResetColor();
 
-        static void Main()
+        Console.Write("Введите имя детектива: ");
+        playerName = Console.ReadLine();
+
+        while (true)
         {
-            Console.WriteLine("=== ТАЙНА ЗАБРОШЕННОГО ПОМЕСТЬЯ ===");
-            Console.Write("Введите имя вашего персонажа: ");
-            playerName = Console.ReadLine();
-
-            Chapter1();
+            Console.Clear();
+            ShowStatus();
+            MainHub();
         }
+    }
 
-        static void Chapter1()
+    static void ShowStatus()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine($"\n{playerName} | CR: {creds} | REP: {reputation}");
+        Console.WriteLine($"Ключи: {string.Join(", ", clues)}");
+        Console.ResetColor();
+    }
+
+    static void MainHub()
+    {
+        ShowHeader("Главное меню");
+        Console.WriteLine("1. Начать новое расследование");
+        Console.WriteLine("2. Посетить черный рынок");
+        Console.WriteLine("3. Проверить архив дел");
+        Console.WriteLine("4. Выйти из игры");
+
+        switch (GetChoice(1, 4))
         {
-            while (true)
-            {
-                Console.WriteLine("\n=== ГЛАВА 1: ПРОБУЖДЕНИЕ ===");
-                Console.WriteLine($"{playerName}, вы просыпаетесь в незнакомой комнате старинного поместья.");
-                Console.WriteLine("1. Осмотреться вокруг");
-                Console.WriteLine("2. Попробовать открыть дверь");
-                Console.WriteLine("3. Кричать о помощи");
-                Console.WriteLine("4. Выйти из игры");
+            case 1: StartInvestigation(); break;
+            case 2: VisitBlackMarket(); break;
+            case 3: CheckCaseFiles(); break;
+            case 4: Environment.Exit(0); break;
+        }
+    }
 
-                switch (GetChoice(1, 4))
+    static void StartInvestigation()
+    {
+        int caseType = rand.Next(1, 4);
+        
+        switch (caseType)
+        {
+            case 1:
+                ShowHeader("Дело #451: Пропавший курьер");
+                Console.WriteLine("Корпорация 'Нео-Экспресс' нанимает вас найти пропавшего сотрудника.");
+                Console.WriteLine("1. Взять дело (100CR)");
+                Console.WriteLine("2. Отказаться");
+
+                if (GetChoice(1, 2) == 1)
                 {
-                    case 1:
-                        Console.WriteLine("\nВ комнате вы видите:");
-                        Console.WriteLine("1. Старый ключ на столе");
-                        Console.WriteLine("2. Записку на подоконнике");
-                        Console.WriteLine("3. Странный символ на стене");
-                        Console.WriteLine("4. Вернуться");
-
-                        switch (GetChoice(1, 4))
-                        {
-                            case 1:
-                                hasKey = true;
-                                Console.WriteLine("Вы взяли ключ. Он выглядит подходящим для двери.");
-                                break;
-                            case 2:
-                                knowsSecret = true;
-                                Console.WriteLine("На записке написано: 'Не доверяйте теням'.");
-                                break;
-                            case 3:
-                                Console.WriteLine("Символ светится при вашем прикосновении!");
-                                trustPoints++;
-                                break;
-                        }
-                        break;
-
-                    case 2:
-                        if (hasKey)
-                        {
-                            Console.WriteLine("\nВы открываете дверь и попадаете в длинный коридор.");
-                            Chapter2();
-                            return;
-                        }
-                        Console.WriteLine("\nДверь заперта. Нужно найти ключ.");
-                        break;
-
-                    case 3:
-                        Console.WriteLine("\nВаши крики привлекли внимание...");
-                        Console.WriteLine("1. Продолжить звать на помощь");
-                        Console.WriteLine("2. Затаиться");
-
-                        if (GetChoice(1, 2) == 1)
-                        {
-                            ShowEnding(1, "Дверь распахивается, и вас хватают темные силуэты...", false);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Шаги проходят мимо. Вы в безопасности... пока.");
-                        }
-                        break;
-
-                    case 4:
-                        Environment.Exit(0);
-                        break;
+                    creds += 100;
+                    InvestigateCourier();
                 }
-            }
-        }
+                break;
 
-        static void Chapter2()
-        {
-            Console.WriteLine("\n=== ГЛАВА 2: КОРИДОР ТАЙН ===");
-            Console.WriteLine("Перед вами три двери и лестница наверх:");
-            Console.WriteLine("1. Красная дверь (помечена черепом)");
-            Console.WriteLine("2. Синяя дверь (слышны голоса)");
-            Console.WriteLine("3. Зеленая дверь (пахнет травами)");
-            Console.WriteLine("4. Подняться по лестнице");
+            case 2:
+                ShowHeader("Дело #327: Взлом банка данных");
+                Console.WriteLine("Неизвестные взломали серверы городского архива.");
+                Console.WriteLine("1. Изучить место преступления");
+                Console.WriteLine("2. Проигнорировать");
 
-            switch (GetChoice(1, 4))
-            {
-                case 1:
-                    RedDoor();
-                    break;
-                case 2:
-                    BlueDoor();
-                    break;
-                case 3:
-                    GreenDoor();
-                    break;
-                case 4:
-                    Stairs();
-                    break;
-            }
-        }
-
-        static void RedDoor()
-        {
-            Console.WriteLine("\nЗа дверью - лаборатория алхимика.");
-            Console.WriteLine("1. Исследовать стол");
-            Console.WriteLine("2. Осмотреть книжную полку");
-            Console.WriteLine("3. Выйти");
-
-            switch (GetChoice(1, 3))
-            {
-                case 1:
-                    if (knowsSecret)
-                    {
-                        ShowEnding(2, "Вы находите эликсир и используете его, чтобы покинуть поместье!", true);
-                    }
-                    else
-                    {
-                        ShowEnding(3, "Вы выпиваете неизвестную жидкость и превращаетесь в статую...", false);
-                    }
-                    break;
-                case 2:
-                    hasMap = true;
-                    Console.WriteLine("Вы нашли карту поместья!");
-                    Chapter2();
-                    break;
-            }
-        }
-
-        static void BlueDoor()
-        {
-            Console.WriteLine("\nВ комнате сидит старик.");
-            Console.WriteLine("1. Заговорить с ним");
-            Console.WriteLine("2. Атаковать его");
-            Console.WriteLine("3. Уйти");
-
-            switch (GetChoice(1, 3))
-            {
-                case 1:
-                    metAlly = true;
-                    Console.WriteLine("Старик оказывается бывшим хозяином поместья. Он дает вам совет.");
-                    trustPoints += 2;
-                    Chapter2();
-                    break;
-                case 2:
-                    ShowEnding(4, "Старик оказывается призраком и забирает вашу душу...", false);
-                    break;
-                case 3:
-                    Chapter2();
-                    break;
-            }
-        }
-
-        static void GreenDoor()
-        {
-            Console.WriteLine("\nВы в оранжерее с экзотическими растениями.");
-            Console.WriteLine("1. Сорвать красный цветок");
-            Console.WriteLine("2. Сорвать синий цветок");
-            Console.WriteLine("3. Выйти");
-
-            switch (GetChoice(1, 3))
-            {
-                case 1:
-                    ShowEnding(5, "Цветок оживает и опутывает вас корнями...", false);
-                    break;
-                case 2:
-                    Console.WriteLine("Цветок дает вам странное видение...");
-                    trustPoints++;
-                    Chapter2();
-                    break;
-                case 3:
-                    Chapter2();
-                    break;
-            }
-        }
-
-        static void Stairs()
-        {
-            Console.WriteLine("\nПоднявшись, вы видите:");
-            Console.WriteLine("1. Дверь в чердак");
-            Console.WriteLine("2. Окно с возможностью выбраться");
-            Console.WriteLine("3. Вернуться");
-
-            switch (GetChoice(1, 3))
-            {
-                case 1:
-                    Attic();
-                    break;
-                case 2:
-                    if (trustPoints >= 3)
-                    {
-                        ShowEnding(6, "Вы выбираетесь через окно и оказываетесь в безопасности!", true);
-                    }
-                    else
-                    {
-                        ShowEnding(7, "Вы падаете и ломаете ногу... Тени приближаются...", false);
-                    }
-                    break;
-                case 3:
-                    Chapter2();
-                    break;
-            }
-        }
-
-        static void Attic()
-        {
-            Console.WriteLine("\nНа чердаке вы находите древний артефакт.");
-            Console.WriteLine("1. Взять его");
-            Console.WriteLine("2. Оставить");
-
-            if (GetChoice(1, 2) == 1)
-            {
-                if (metAlly && hasMap)
+                if (GetChoice(1, 2) == 1)
                 {
-                    ShowEnding(8, "Артефакт переносит вас домой! Вы спасены!", true);
+                    InvestigateHack();
                 }
-                else
-                {
-                    ShowEnding(9, "Артефакт поглощает ваше сознание...", false);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Вы чувствуете, что сделали правильный выбор.");
-                trustPoints++;
-                FinalChoice();
-            }
-        }
+                break;
 
-        static void FinalChoice()
+            case 3:
+                ShowHeader("Дело #198: Подозрительное самоубийство");
+                Console.WriteLine("Офицер корпоративной безопасности найден мертвым.");
+                Console.WriteLine("1. Осмотреть тело");
+                Console.WriteLine("2. Закрыть дело");
+
+                if (GetChoice(1, 2) == 1)
+                {
+                    InvestigateSuicide();
+                }
+                break;
+        }
+    }
+
+    static void InvestigateCourier()
+    {
+        Console.WriteLine("\nВы начинаете расследование...");
+        Console.WriteLine("1. Опросить коллег");
+        Console.WriteLine("2. Проверить последний маршрут");
+
+        int choice = GetChoice(1, 2);
+        
+        if (choice == 1)
         {
-            Console.WriteLine("\n=== ФИНАЛЬНЫЙ ВЫБОР ===");
-            Console.WriteLine("1. Попытаться найти другой выход");
-            Console.WriteLine("2. Вернуться к старику за советом");
-
-            if (GetChoice(1, 2) == 1)
-            {
-                if (trustPoints >= 4)
-                {
-                    ShowEnding(10, "Используя все полученные знания, вы находите тайный выход!", true);
-                }
-                else
-                {
-                    Console.WriteLine("Вы блуждаете по коридорам...");
-                    ShowEnding(11, "Поместье не отпускает вас... Вы становитесь его частью.", false);
-                }
-            }
-            else
-            {
-                if (metAlly)
-                {
-                    ShowEnding(12, "Старик раскрывает вам последний секрет и помогает сбежать!", true);
-                }
-                else
-                {
-                    ShowEnding(13, "Старика нигде нет... Вы остаетесь один в темноте...", false);
-                }
-            }
+            Console.WriteLine("\nКоллеги говорят, что курьер боялся чего-то...");
+            clues.Add("Курьер в страхе");
         }
-
-        static void ShowEnding(int number, string message, bool isGood)
+        else
         {
-            Console.WriteLine($"\n=== КОНЦОВКА #{number} ===");
-            Console.WriteLine(message);
-            Console.WriteLine(isGood ? "★ ХОРОШАЯ КОНЦОВКА ★" : "✖ ПЛОХАЯ КОНЦОВКА ✖");
-            Console.WriteLine("\nСпасибо за игру!");
-            Console.ReadKey();
-            Environment.Exit(0);
+            Console.WriteLine("\nНа маршруте обнаружены следы борьбы!");
+            clues.Add("Следы борьбы");
         }
 
-        static int GetChoice(int min, int max)
+        if (rand.Next(100) > 50)
         {
-            int choice;
-            while (true)
+            Console.WriteLine("\nВы нашли курьера живым!");
+            creds += 200;
+            reputation += 2;
+        }
+        else
+        {
+            Console.WriteLine("\nКурьер мертв. Дело закрыто.");
+            reputation -= 1;
+        }
+
+        Console.ReadKey();
+    }
+
+    static void VisitBlackMarket()
+    {
+        ShowHeader("Черный рынок");
+        Console.WriteLine("1. Купить оружие (200CR)");
+        Console.WriteLine("2. Купить информацию (50CR)");
+        Console.WriteLine("3. Продать данные (+100CR)");
+        Console.WriteLine("4. Вернуться");
+
+        int choice = GetChoice(1, 4);
+        
+        switch (choice)
+        {
+            case 1 when creds >= 200:
+                creds -= 200;
+                hasWeapon = true;
+                Console.WriteLine("\nПриобретен кибер-пистолет!");
+                break;
+            case 2 when creds >= 50:
+                creds -= 50;
+                clues.Add("Рынок: " + rand.Next(1000, 9999).ToString());
+                Console.WriteLine("\nПолучены сомнительные данные...");
+                break;
+            case 3:
+                creds += 100;
+                if (clues.Count > 0) clues.RemoveAt(0);
+                Console.WriteLine("\nДанные проданы. Кто-то недоволен...");
+                reputation -= 1;
+                break;
+        }
+
+        Console.ReadKey();
+    }
+
+    static void CheckCaseFiles()
+    {
+        ShowHeader("Архив дел");
+        if (clues.Count == 0)
+        {
+            Console.WriteLine("Нет собранных улик.");
+        }
+        else
+        {
+            Console.WriteLine("Собранные улики:");
+            foreach (var clue in clues)
             {
-                Console.Write("Ваш выбор: ");
-                if (int.TryParse(Console.ReadLine(), out choice) && choice >= min && choice <= max)
-                {
-                    return choice;
-                }
-                Console.WriteLine($"Пожалуйста, введите число от {min} до {max}.");
+                Console.WriteLine("- " + clue);
             }
         }
+        Console.ReadKey();
+    }
+
+    static int GetChoice(int min, int max)
+    {
+        int choice;
+        do
+        {
+            Console.Write("> ");
+        } while (!int.TryParse(Console.ReadLine(), out choice) || choice < min || choice > max);
+        
+        return choice;
+    }
+
+    static void ShowHeader(string title)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine($"\n─── {title} ───");
+        Console.ResetColor();
     }
 }
